3fd4e152b625c6916bab736ba6a3f9788f8d21be
Gokuldas-dev <gokuldasshenoy2@gmail.com>
2024-04-18 09:44:28 +0530

    .

---
+++ indexing_in_python.py
@@ -0,0 +1,3 @@
+#This is an example for indexing 
+course = 'python is for beginners'
+print(course[0:3])
---
+++ string_exc.py
@@ -0,0 +1,12 @@
+course = 'python for "beginners"'
+print (course)
+
+#this is a single line with double quotes
+course_1 = '''HI john 
+This is an  Eamil sended to you 
+
+Thankyou
+The support team'''
+
+print (course_1)
+#This is a tripple qutes so we can send and use for like email example
